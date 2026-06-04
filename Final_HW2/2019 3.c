87c47b5168fa48edce867733bea9baeed7d7a706
kimnahoe <sukjinjin921204@gmail.com>
2026-06-04 10:51:31 +0900

    Refactor one_memo function and memory allocation

--- Final_HW2/2019 3.c
+++ Final_HW2/2019 3.c
@@ -1,46 +1,67 @@
+//애초에 함수 보낼 때 초기 함수랑 같이 보내서.. 같은거 있으면 그대로 없으면 계산 수행..?
 #include <stdio.h>
 #include <stdlib.h>
 
-int one_memo(int n, int *m) {
+int one_memo(int n, int* m) {
 	int i, ret;
-	int count=0;
 
-	if(m[n] != 0) //no same
+	if (m[n] != 0) //no same
 		return m[n];
+		
+	if (n == 1) //end
+		return 1;
+		
+	if (n % 3 == 0)
+		ret = 1 + one_memo(n/3, m);
+	else if (n % 2 == 0)
+		ret = 1 + one_memo(n/2, m);
+	else
+		ret = 1 + one_memo(n-1, m);
 
-	if(n == 1) //end
-		return count;
-
-	ret = 0;
-	for(i=n; i>=1; i--) {
-		if(n%3==0)
-			n/=3;
-		else if(n%2==0)
-			n/=2;
-		else
-			n-=1;
-		ret = ret + one_memo(i, m);
-		count++;
-	}
-	
 	m[n] = ret;
-	
+
 	return ret;
-	
+
 }
 
+
 int main() {
-	int n,i;
-	int *M;
+	int n, i;
+	int* M, * m2, * m3;
+	int max = -1;
 
 	scanf("%d", &n);
-	M = (int*)malloc(sizeof(int)*(n+1));
-	
-	for(i=0; i<n+1; i++)
+
+	M = (int*)malloc(sizeof(int) * 100);
+	for (i = 0; i < n; i++)
 		M[i] = 0;
-	printf("%d\n", one_memo(n, M));
+	int mm=one_memo(n, M);
+
+	m2 = (int*)malloc(sizeof(int) * 100);
+	for (i = 0; i < n; i++)
+		m2[i] = 0;
+	int m2c = one_memo(n, m2);
+
+	if (mm < m2c)
+		max = m2c;
+	else
+		max = mm;
+
+	m3 = (int*)malloc(sizeof(int) * 100);
+	for (i = 0; i < n; i++)
+		m3[i] = 0;
+	int m3c = one_memo(n, m3);
+
+	if (mm < m3c)
+		max = m3c;
+	else
+		max = mm;
+
+	printf("%d", max);
 
 	free(M);
+	free(m2);
+	free(m3);
 
 	return 0;
 }
