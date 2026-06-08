f535439106efeb919cb2333c6129fd7954e2ab5c
nahoe Kim <sukjinjin921204@gmail.com>
2026-06-09 00:01:58 +0900

    Update 2024 5-2.c

--- Final_HW2/2024 5-2.c
+++ Final_HW2/2024 5-2.c
@@ -1,51 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-//다시 풀어야 됨!!!!!!!!
-int start1(int n, int *M) {
+//backpointer 활용하기
+
+int start1(int n, int *M, int *bp) {
 	int min=10000000;
 	int ret=0;
+	
 	
 	if(M[n] != 0)
 		return M[n];
 		
-	if(n == 1) {
-		printf("1");
+	if(n == 1)
 		return 0;
-	}
-	else {
-		printf("%d ", n);
+	else {		
 		if(n%3==0) {
-			ret = start1(n/3, M) + 1;
-			if(ret<min)
+			ret = start1(n/3, M, bp) + 1;
+			if(ret<min) {
 				min=ret;
+				bp[n]=n/3;
+			}
 		}
-		else if(n%2==0) {
-			ret = start1(n/2, M) + 1;
-			if(ret<min)
+		if(n%2==0) {
+			ret = start1(n/2, M, bp) + 1;
+			if(ret<min) {
 				min=ret;
+				bp[n]=n/2;
+			}
+			
 		}
-		else {
-			ret = start1(n-1, M) + 1;
-			if(ret<min)
+		if(n != 1) {
+			ret = start1(n-1, M, bp) + 1;
+			if(ret<min) {
 				min=ret;
+				bp[n]=n-1;
+			}
 		}
-		M[n] = ret;
+			
+		M[n] = min;
+				
 		return min;
 	}
 }
 
 int main() {
 	int n,i;
-	int *M;
+	int *M, *bp;
 
 	scanf("%d", &n);
 	
 	M = (int*)malloc(sizeof(int)*(n+1));
+	bp = (int*)malloc(sizeof(int)*(n+1));
 
-	for(i=0; i<=n; i++)
+	for(i=0; i<=n; i++) {
     	M[i] = 0;
+    	bp[i] = 0;
+    }
 
-	printf("\n%d", start1(n, M));
+	printf("%d\n", start1(n, M, bp));
+	
+	int num=n;
+	
+	while(1) {
+		printf("%d ", num);
+		if(num == 1)
+			break;
+		num=bp[num];
+	}
 
 	free(M);
 
