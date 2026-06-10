b8545e6f82f32d535340c1156d3d66427b5d4aa5
nahoe Kim <sukjinjin921204@gmail.com>
2026-06-11 00:23:42 +0900

    Update meQ7.c

--- midterm exam/meQ7.c
+++ midterm exam/meQ7.c
@@ -10,21 +10,23 @@ int pick(int *item, int itemSize, int* bucket, int bucketSize, int k, int key)
 	if (k == 0) {
 		for (i = 0; i < bucketSize; i++)
 			sum += item[bucket[i]];
-		/*if(sum == key) {
-			for(i=0; i<bucketSize; i++)
-				printf("%d ", item[bucket[i]]);
-		}
-		printf("\n");
-		*/
 		return sum==key?1:0;
 	}
 	
-	lastIndex = bucketSize - k - 1; // 가장 최근에 뽑힌 수가 저장된 위치 index
+	lastIndex = bucketSize - k - 1;
+
+	for (i = 0; i <= lastIndex; i++)
+		sum += item[bucket[i]]; //중간 중간 확인해주기
+		
+	if(sum > key) //가지치기 sum보다 많으면 그냥 바로 탈락
+		return 0;
+	else if(sum == key) //가지치기 sum 같으면 바로 성공
+		return 1;
 
 	if (bucketSize == k)
 		smallest = 0;
 	else
-		smallest = bucket[lastIndex]; // 이부분만 combination과 차이
+		smallest = bucket[lastIndex];
 
 	for (i = smallest; i < itemSize; i++) {
 		bucket[lastIndex + 1] = i;
@@ -40,13 +42,13 @@ int main(void)
 	scanf("%d", &n);
 	scanf("%d", &k);
 	
-	int *bucket=(int*)malloc(sizeof(int)*n);
+	int *bucket=(int*)malloc(sizeof(int)*k); //1만 다 뽑는 경우 요소가 가장 많음 즉, k만큼 뽑기 가능
 	int *arr=(int*)malloc(sizeof(int)*n);
 	
 	for(i=0; i<n; i++)
 		scanf("%d", &arr[i]);
-		
-	printf("%d", pick(arr, k, bucket, k, k, k));
+	
+	printf("%d", pick(arr, n, bucket, k, k, k));
 		
 	free(arr);
 	free(bucket);
